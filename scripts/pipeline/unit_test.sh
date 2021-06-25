46c2c918a76c8a033059a9ba625c58aef4d2aba5
wayonb <wayonb@gmail.com>
2021-06-25 16:20:32 -0400

    update

--- scripts/pipeline/unit_test.sh
+++ scripts/pipeline/unit_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 SUBPROJECT = $1
-cd ${SUBPROJECT} && yarn run lint && yarn run test:travis && cd ..
+echo $(pwd;ls -la)
+cd ${SUBPROJECT} && yarn run lint && yarn run test:jenkins && cd ..
